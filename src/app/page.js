a80e72cf73dfc1f17fc557ef850afb99f54eebf3
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-01-10 23:29:49 +0600

    create our Blog page

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Header from "../components/shared/Header.jsx";
 import AboutUs from "../components/ui/AboutUs.jsx";
 import FruitBundle from "../components/ui/FruitBundle.jsx";
 import Testimonial from "../components/ui/Testimonial.jsx";
+import OurBlog from "../components/ui/OurBlog.jsx";
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
    <AboutUs/>
    <FruitBundle/>
    <Testimonial/>
+   <OurBlog/>
     </div>
   );
 }
